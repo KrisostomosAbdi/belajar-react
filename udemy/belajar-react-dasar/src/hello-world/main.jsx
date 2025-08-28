7769e9de5faabf2b88ebfb315e471f1f211fc50f
Krisostomos-Abdi <98306652+Krisostomos-Abdi@users.noreply.github.com>
2025-08-28 09:43:57 +0700

    added table and rows view

--- udemy/belajar-react-dasar/src/hello-world/main.jsx
+++ udemy/belajar-react-dasar/src/hello-world/main.jsx
@@ -3,6 +3,7 @@ import HelloWorld from "./HelloWorld";
 import { StrictMode } from "react";
 import Container from './Container.jsx'
 import ToDoList from "../todolist/todolist.jsx";
+import Table from "../table/Table.jsx";
 
 createRoot(document.getElementById("root"))
     .render(
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root"))
             <Container>
                 <HelloWorld/>
                 <ToDoList/>
+                <Table/>
             </Container>
         </StrictMode>
     );
